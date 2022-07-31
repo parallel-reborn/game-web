e0226b79d00de748db4c76061958e4d1aa6df856
kevin <kevin.draym@gmail.com>
2022-07-31 18:33:05 +0800

    task: setup main views

---
+++ pages/game/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Game: NextPage = () => {
+  return (
+    <div className="container">
+      <Head>
+        <title>Reborn</title>
+        <meta name="description" content="Reborn.io" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+
+      <main>
+          Coming soon..
+      </main>
+
+      <footer>
+        <a href="https://github.com/parallel-reborn" target="_blank" rel="noopener noreferrer">
+          Powered by Reborn.io
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Game
